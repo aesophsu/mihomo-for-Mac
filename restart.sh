ef3ca5f47fb806cc6a3259862125e9a05d6e9041
Akkariin Meiko <akkariins@gmail.com>
2025-03-13 20:06:17 +0800

    Update

--- restart.sh
+++ restart.sh
@@ -41,16 +41,16 @@ Server_Dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 Conf_Dir="$Server_Dir/conf"
 Log_Dir="$Server_Dir/logs"
 
-## 关闭clash服务
+## 关闭mihomo服务
 Text1="服务关闭成功！"
 Text2="服务关闭失败！"
 # 查询并关闭程序进程
-PID_NUM=`ps -ef | grep [c]lash-linux-a | wc -l`
-PID=`ps -ef | grep [c]lash-linux-a | awk '{print $2}'`
+PID_NUM=`ps -ef | grep [m]ihomo-linux-a | wc -l`
+PID=`ps -ef | grep [m]ihomo-linux-a | awk '{print $2}'`
 if [ $PID_NUM -ne 0 ]; then
 	kill -9 $PID
   ReturnStatus=$?
-	# ps -ef | grep [c]lash-linux-a | awk '{print $2}' | xargs kill -9
+	# ps -ef | grep [m]ihomo-linux-a | awk '{print $2}' | xargs kill -9
 fi
 if_success $Text1 $Text2 $ReturnStatus
 
@@ -68,19 +68,19 @@ else
 	exit 1
 fi
 
-## 重启启动clash服务
+## 重启启动mihomo服务
 Text5="服务启动成功！"
 Text6="服务启动失败！"
 if [[ $CpuArch =~ "x86_64" ]]; then
-	nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/mihomo-linux-amd64 -d $Conf_Dir &> $Log_Dir/mihomo.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 elif [[ $CpuArch =~ "aarch64" ||  $CpuArch =~ "arm64" ]]; then
-	nohup $Server_Dir/bin/clash-linux-arm64 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/mihomo-linux-arm64 -d $Conf_Dir &> $Log_Dir/mihomo.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 elif [[ $CpuArch =~ "armv7" ]]; then
-	nohup $Server_Dir/bin/clash-linux-armv7 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/mihomo-linux-armv7 -d $Conf_Dir &> $Log_Dir/mihomo.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 else
--- start.sh
+++ start.sh
@@ -94,29 +94,29 @@ unset HTTPS_PROXY
 unset NO_PROXY
 
 
-## Clash 订阅地址检测及配置文件下载
+## Mihomo 订阅地址检测及配置文件下载
 # 检查url是否有效
 echo -e '\n正在检测订阅地址...'
-Text1="Clash订阅地址可访问！"
-Text2="Clash订阅地址不可访问！"
+Text1="Mihomo订阅地址可访问！"
+Text2="Mihomo订阅地址不可访问！"
 #curl -o /dev/null -s -m 10 --connect-timeout 10 -w %{http_code} $URL | grep '[23][0-9][0-9]' &>/dev/null
 curl -o /dev/null -L -k -sS --retry 5 -m 10 --connect-timeout 10 -w "%{http_code}" $URL | grep -E '^[23][0-9]{2}$' &>/dev/null
 ReturnStatus=$?
 if_success $Text1 $Text2 $ReturnStatus
 
 # 拉取更新config.yml文件
-echo -e '\n正在下载Clash配置文件...'
+echo -e '\n正在下载Mihomo配置文件...'
 Text3="配置文件config.yaml下载成功！"
 Text4="配置文件config.yaml下载失败，退出启动！"
 
 # 尝试使用curl进行下载
-curl -L -k -sS --retry 5 -m 10 -o $Temp_Dir/clash.yaml $URL
+curl -L -k -sS --retry 5 -m 10 -o $Temp_Dir/mihomo.yaml $URL
 ReturnStatus=$?
 if [ $ReturnStatus -ne 0 ]; then
 	# 如果使用curl下载失败，尝试使用wget进行下载
 	for i in {1..10}
 	do
-		wget -q --no-check-certificate -O $Temp_Dir/clash.yaml $URL
+		wget -q --no-check-certificate -O $Temp_Dir/mihomo.yaml $URL
 		ReturnStatus=$?
 		if [ $ReturnStatus -eq 0 ]; then
 			break
@@ -127,49 +127,49 @@ if [ $ReturnStatus -ne 0 ]; then
 fi
 if_success $Text3 $Text4 $ReturnStatus
 
-# 重命名clash配置文件
-\cp -a $Temp_Dir/clash.yaml $Temp_Dir/clash_config.yaml
+# 重命名mihomo配置文件
+\cp -a $Temp_Dir/mihomo.yaml $Temp_Dir/mihomo_config.yaml
 
 
-## 判断订阅内容是否符合clash配置文件标准，尝试转换（当前不支持对 x86_64 以外的CPU架构服务器进行clash配置文件检测和转换，此功能将在后续添加）
+## 判断订阅内容是否符合mihomo配置文件标准，尝试转换（当前不支持对 x86_64 以外的CPU架构服务器进行mihomo配置文件检测和转换，此功能将在后续添加）
 if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64"  ]]; then
-	echo -e '\n判断订阅内容是否符合clash配置文件标准:'
-	bash $Server_Dir/scripts/clash_profile_conversion.sh
+	echo -e '\n判断订阅内容是否符合mihomo配置文件标准:'
+	bash $Server_Dir/scripts/mihomo_profile_conversion.sh
 	sleep 3
 fi
 
 
-## Clash 配置文件重新格式化及配置
+## Mihomo 配置文件重新格式化及配置
 # 取出代理相关配置 
-#sed -n '/^proxies:/,$p' $Temp_Dir/clash.yaml > $Temp_Dir/proxy.txt
-sed -n '/^proxies:/,$p' $Temp_Dir/clash_config.yaml > $Temp_Dir/proxy.txt
+#sed -n '/^proxies:/,$p' $Temp_Dir/mihomo.yaml > $Temp_Dir/proxy.txt
+sed -n '/^proxies:/,$p' $Temp_Dir/mihomo_config.yaml > $Temp_Dir/proxy.txt
 
 # 合并形成新的config.yaml
 cat $Temp_Dir/templete_config.yaml > $Temp_Dir/config.yaml
 cat $Temp_Dir/proxy.txt >> $Temp_Dir/config.yaml
 \cp $Temp_Dir/config.yaml $Conf_Dir/
 
-# Configure Clash Dashboard
+# Configure Mihomo Dashboard
 Work_Dir=$(cd $(dirname $0); pwd)
 Dashboard_Dir="${Work_Dir}/dashboard/public"
 sed -ri "s@^# external-ui:.*@external-ui: ${Dashboard_Dir}@g" $Conf_Dir/config.yaml
 sed -r -i '/^secret: /s@(secret: ).*@\1'${Secret}'@g' $Conf_Dir/config.yaml
 
 
-## 启动Clash服务
-echo -e '\n正在启动Clash服务...'
+## 启动Mihomo服务
+echo -e '\n正在启动Mihomo服务...'
 Text5="服务启动成功！"
 Text6="服务启动失败！"
 if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64"  ]]; then
-	nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/mihomo-linux-amd64 -d $Conf_Dir &> $Log_Dir/mihomo.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 elif [[ $CpuArch =~ "aarch64" ||  $CpuArch =~ "arm64" ]]; then
-	nohup $Server_Dir/bin/clash-linux-arm64 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/mihomo-linux-arm64 -d $Conf_Dir &> $Log_Dir/mihomo.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 elif [[ $CpuArch =~ "armv7" ]]; then
-	nohup $Server_Dir/bin/clash-linux-armv7 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/mihomo-linux-armv7 -d $Conf_Dir &> $Log_Dir/mihomo.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 else
@@ -179,12 +179,12 @@ fi
 
 # Output Dashboard access address and Secret
 echo ''
-echo -e "Clash Dashboard 访问地址: http://<ip>:9090/ui"
+echo -e "Mihomo Dashboard 访问地址: http://<ip>:9090/ui"
 echo -e "Secret: ${Secret}"
 echo ''
 
 # 添加环境变量(root权限)
-cat>/etc/profile.d/clash.sh<<EOF
+cat>/etc/profile.d/mihomo.sh<<EOF
 # 开启系统代理
 function proxy_on() {
 	export http_proxy=http://127.0.0.1:7890
@@ -208,6 +208,6 @@ function proxy_off(){
 }
 EOF
 
-echo -e "请执行以下命令加载环境变量: source /etc/profile.d/clash.sh\n"
+echo -e "请执行以下命令加载环境变量: source /etc/profile.d/mihomo.sh\n"
 echo -e "请执行以下命令开启系统代理: proxy_on\n"
 echo -e "若要临时关闭系统代理，请执行: proxy_off\n"
