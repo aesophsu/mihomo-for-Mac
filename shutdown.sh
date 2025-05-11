985537187f43c902727be900badd156e94741e1b
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-05-11 18:45:28 +0800

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# 关闭mihomo服务
-PID_NUM=`ps -ef | grep [m]ihomo-linux-a | wc -l`
-PID=`ps -ef | grep [m]ihomo-linux-a | awk '{print $2}'`
-if [ $PID_NUM -ne 0 ]; then
-	kill -9 $PID
-	# ps -ef | grep [m]ihomo-linux-a | awk '{print $2}' | xargs kill -9
+# 获取 Mihomo 进程 ID
+PID=$(ps -ef | grep "[m]ihomo -d" | awk '{print $2}')
+
+# 判断是否有 Mihomo 进程在运行
+if [ -n "$PID" ]; then
+    echo "正在关闭 Mihomo 服务 (PID: $PID)..."
+    kill -9 $PID
+    echo "Mihomo 服务已关闭。"
+else
+    echo "Mihomo 服务未运行。"
 fi
 
-# 清除环境变量
-> /etc/profile.d/mihomo.sh
-
-echo -e "\n服务关闭成功，请执行以下命令关闭系统代理：proxy_off\n"
+echo -e "\n服务关闭成功，请执行以下命令关闭系统代理：source $Server_Dir/proxy.sh && proxy_off\n"
